9017a651ca8f3c934570de8d9b9cab0194e7fd4e
Daniel <daniel_flebbe@yahoo.de>
2020-06-16 11:43:20 +0200

    Add + Script 1.3, 1.4

--- 01_Introduction.R
+++ 01_Introduction.R
@@ -1,8 +1,48 @@
-#Chapter 01: Introduction
-
-#Script 1.1
+##### Chapter 01: Introduction 
+#### Script 1.1 ####
 1+1
 5*(4-1)^2
 sqrt(log(10))
 
 
+#### Script 1.3 "Objects" ####
+
+# generate object x
+x <- 5
+
+# display and calculate with x
+x
+x^2
+
+# generate objects
+
+y <- 3
+y
+
+z <- y^x
+z
+
+# display all defined variables
+ls()
+exists("x")
+
+# remove
+rm(x) # nur x
+rm(list = ls()) #alle
+
+
+#### Script 1.4 "Vectors" ####
+# Define a with immediate output through parentheses:
+c(1,2,3,4,5,6)
+
+(a <- c(1,2,3,4,5,6))
+
+(b <- a+1)
+
+(c <- a+b)
+
+(d <- b*c)
+
+sqrt(d)
+
+#### Script 1.5 "Vector-Functions" ####
